4dfbde2c6e0b37300387dd0b1d82b401dc2df82f
zhenyu zhong <genericname001@gmail.com>
2020-09-14 20:01:52 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
+
+export { app };
